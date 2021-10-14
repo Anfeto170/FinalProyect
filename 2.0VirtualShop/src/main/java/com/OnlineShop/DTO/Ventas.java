353dc9abc724289651921acc8e6006e26c270122
Andrés Torres <anfeto334@gmail.com>
2021-10-14 16:03:16 -0500

    ventas

--- 2.0VirtualShop/src/main/java/com/OnlineShop/DTO/Ventas.java
+++ 2.0VirtualShop/src/main/java/com/OnlineShop/DTO/Ventas.java
@@ -2,11 +2,20 @@ package com.OnlineShop.DTO;
 
 public class Ventas {
 
-	private String codigo_producto;
+	private String Codigo_producto;
 	private int cantidad;
 	private float Total;
 	private String NIT_cliente, Fecha;
 
+	public Ventas(String nIT_cliente, String codigo_producto, int cantidad, float total, String fecha) {
+		super();
+		this.Codigo_producto = codigo_producto;
+		this.cantidad = cantidad;
+		this.NIT_cliente = nIT_cliente;
+		this.Total = total;
+		this.Fecha = fecha;
+	}
+	
 	public String getFecha() {
 		return Fecha;
 	}
@@ -16,11 +25,11 @@ public class Ventas {
 	}
 
 	public String getCodigo_producto() {
-		return codigo_producto;
+		return Codigo_producto;
 	}
 
 	public void setCodigo_producto(String codigo_producto) {
-		this.codigo_producto = codigo_producto;
+		this.Codigo_producto = codigo_producto;
 	}
 
 	public int getCantidad() {
@@ -47,13 +56,6 @@ public class Ventas {
 		NIT_cliente = nIT_cliente;
 	}
 
-	public Ventas(String nIT_cliente, String codigo_producto, int cantidad, float total, String fecha) {
-		super();
-		this.codigo_producto = codigo_producto;
-		this.cantidad = cantidad;
-		this.NIT_cliente = nIT_cliente;
-		this.Total = total;
-		this.Fecha = fecha;
-	}
+
 
 }
